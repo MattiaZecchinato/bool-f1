8a4cb1762147e3e6e1044df0fd8499c9bba1bf20
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-09-19 16:04:44 +0200

    feat: add show path

--- backend/src/main/java/org/boolf1/backend/controller/DriverController.java
+++ backend/src/main/java/org/boolf1/backend/controller/DriverController.java
@@ -1,11 +1,13 @@
 package org.boolf1.backend.controller;
 
+import org.boolf1.backend.repository.DriverRepository;
 import org.boolf1.backend.service.DriverService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -21,5 +23,13 @@ public class DriverController {
         model.addAttribute("driversList", driverService.getAll());
 
         return "driver/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") Integer driverId, Model model) {
+
+        model.addAttribute("driverStats", driverService.getById(driverId));
+
+        return "driver/driverStatistics";
     }
 }
