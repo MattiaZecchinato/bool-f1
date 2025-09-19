21553751fd41033c19278cb4ea6dc6dbe1f3c248
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-09-19 13:01:01 +0200

    feat: create team repository

---
+++ backend/src/main/java/org/boolf1/backend/repository/TeamRepository.java
@@ -0,0 +1,8 @@
+package org.boolf1.backend.repository;
+
+import org.boolf1.backend.model.Team;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TeamRepository extends JpaRepository<Team, Integer> {
+
+}
