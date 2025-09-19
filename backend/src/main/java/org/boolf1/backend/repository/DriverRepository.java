3a3d2d6d0c6765abf8d87d7bf63901ffb76ee8ce
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-09-19 14:00:40 +0200

    fix: import Driver entity

--- backend/src/main/java/org/boolf1/backend/repository/DriverRepository.java
+++ backend/src/main/java/org/boolf1/backend/repository/DriverRepository.java
@@ -1,7 +1,6 @@
 package org.boolf1.backend.repository;
 
-import java.sql.Driver;
-
+import org.boolf1.backend.model.Driver;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DriverRepository extends JpaRepository<Driver, Integer> {
