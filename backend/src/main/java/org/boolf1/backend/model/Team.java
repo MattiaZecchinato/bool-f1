32308467dd810acd23dfe2c6aac227ed45cdbe2b
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-09-19 12:56:48 +0200

    feat: create team entity

---
+++ backend/src/main/java/org/boolf1/backend/model/Team.java
@@ -0,0 +1,84 @@
+package org.boolf1.backend.model;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "teams")
+public class Team {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank(message = "Name must be not blank or empty")
+    private String name;
+
+    @NotBlank(message = "Nationality must be not blank or empty")
+    private String nationality;
+
+    @NotNull(message = "Entry year must be not null")
+    @Column(name = "entry_year")
+    private LocalDate entryYear;
+
+    @Column(name = "logo_img")
+    private String logoImg;
+
+    @Column(name = "car_img")
+    private String carImg;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public LocalDate getEntryYear() {
+        return entryYear;
+    }
+
+    public void setEntryYear(LocalDate entryYear) {
+        this.entryYear = entryYear;
+    }
+
+    public String getLogoImg() {
+        return logoImg;
+    }
+
+    public void setLogoImg(String logoImg) {
+        this.logoImg = logoImg;
+    }
+
+    public String getCarImg() {
+        return carImg;
+    }
+
+    public void setCarImg(String carImg) {
+        this.carImg = carImg;
+    }
+}
