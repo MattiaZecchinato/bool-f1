ea670bddc64cfbd47832a41a7d7bd4fa3e573aa8
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-09-19 13:00:12 +0200

    feat: create driver repository

---
+++ backend/src/main/java/org/boolf1/backend/repository/DriverRepository.java
@@ -0,0 +1,9 @@
+package org.boolf1.backend.repository;
+
+import java.sql.Driver;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DriverRepository extends JpaRepository<Driver, Integer> {
+
+}
