29192442f40745a385b617cbb2ee0321928841e6
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-09-23 11:07:45 +0200

    feat: add show path

--- backend/src/main/java/org/boolf1/backend/controller/TeamController.java
+++ backend/src/main/java/org/boolf1/backend/controller/TeamController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -21,5 +22,13 @@ public class TeamController {
         model.addAttribute("teamsList", teamService.getAll());
 
         return "team/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") Integer teamId, Model model) {
+
+        model.addAttribute("teamStats", teamService.getById(teamId));
+
+        return "team/teamStatistics";
     }
 }
