16e7a40149ab0bd0d610ea24913eab5fb8899e81
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-09-23 17:15:56 +0200

    feat: update teamService

--- backend/src/main/java/org/boolf1/backend/service/TeamService.java
+++ backend/src/main/java/org/boolf1/backend/service/TeamService.java
@@ -1,5 +1,8 @@
 package org.boolf1.backend.service;
 
+import java.util.List;
+
+import org.boolf1.backend.model.Team;
 import org.boolf1.backend.repository.TeamRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,4 +11,24 @@ import org.springframework.stereotype.Service;
 public class TeamService {
     @Autowired
     private TeamRepository teamRepository;
+
+    public List<Team> getAll() {
+        return teamRepository.findAll();
+    }
+
+    public Team getById(Integer id) {
+        return teamRepository.findById(id).get();
+    }
+
+    public Team store(Team team) {
+        return teamRepository.save(team);
+    }
+
+    public Team update(Team team) {
+        return teamRepository.save(team);
+    }
+
+    public void deleteById(Integer driverId) {
+        teamRepository.deleteById(driverId);
+    }
 }
