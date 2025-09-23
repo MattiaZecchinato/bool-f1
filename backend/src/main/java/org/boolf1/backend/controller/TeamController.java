ee795e62a4f4780441f03879a480d615f9f5b4b7
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-09-23 11:20:27 +0200

    feat: add create path

--- backend/src/main/java/org/boolf1/backend/controller/TeamController.java
+++ backend/src/main/java/org/boolf1/backend/controller/TeamController.java
@@ -1,13 +1,19 @@
 package org.boolf1.backend.controller;
 
+import org.boolf1.backend.model.Team;
 import org.boolf1.backend.service.TeamService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.validation.Valid;
 
 @Controller
 @CrossOrigin
@@ -30,5 +36,28 @@ public class TeamController {
         model.addAttribute("teamStats", teamService.getById(teamId));
 
         return "team/teamStatistics";
+    }
+
+    @GetMapping("/create")
+    public String create(Model model) {
+
+        model.addAttribute("team", new Team());
+        model.addAttribute("create", true);
+
+        return "team/createOrEdit";
+    }
+
+    @PostMapping("/create")
+    public String store(@Valid @ModelAttribute("team") Team formTeam, BindingResult bindingResult,
+            Model model) {
+
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("create", true);
+            return "team/createOrEdit";
+        }
+
+        teamService.store(formTeam);
+
+        return "redirect:/teams";
     }
 }
