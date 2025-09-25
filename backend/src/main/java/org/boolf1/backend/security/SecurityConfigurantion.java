d29b466770619eabea39a5fb877f6cf361a257de
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-09-25 17:48:38 +0200

    feat: create SecurityConfiguration and add filter chain

---
+++ backend/src/main/java/org/boolf1/backend/security/SecurityConfigurantion.java
@@ -0,0 +1,25 @@
+package org.boolf1.backend.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfigurantion {
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(requests -> requests
+                .requestMatchers("/drivers/create", "/drivers/update/**").hasAuthority("ADMIN")
+                .requestMatchers(HttpMethod.POST, "/drivers/**").hasAuthority("ADMIN")
+                .requestMatchers("/teams/create", "/teams/update/**").hasAuthority("ADMIN")
+                .requestMatchers(HttpMethod.POST, "/teams/**").hasAuthority("ADMIN")
+                .anyRequest().permitAll())
+                .formLogin(Customizer.withDefaults())
+                .cors(cors -> cors.disable())
+                .csrf(csrf -> csrf.disable());
+        return http.build();
+    }
+}
