3cd542378d7292dac9e8a4e8e70182694fb6ab9c
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-09-27 03:06:21 +0200

    feat: create DriverStats component

---
+++ frontend/src/components/DriverStats.jsx
@@ -0,0 +1,19 @@
+function DriverStats({ data }) {
+
+    const { podium, wins, poles, wdc } = data
+
+    return (
+        <>
+            <h2>STATISTICS</h2>
+            <div className="info-stats-box">
+                <h3>CAREER STATS</h3>
+                <p>Podium: {podium}</p>
+                <p>Wins: {wins}</p>
+                <p>Poles: {poles}</p>
+                <p>Wdc: {wdc}</p>
+            </div>
+        </>
+    )
+}
+
+export default DriverStats
