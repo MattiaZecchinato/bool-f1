c09cbbd58e2a8ddba848ed948169499856426c1c
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-09-27 03:23:19 +0200

    fix: change name variable podium to podiums

--- frontend/src/components/DriverStats.jsx
+++ frontend/src/components/DriverStats.jsx
@@ -1,13 +1,13 @@
 function DriverStats({ data }) {
 
-    const { podium, wins, poles, wdc } = data
+    const { podiums, wins, poles, wdc } = data
 
     return (
         <>
             <h2>STATISTICS</h2>
             <div className="info-stats-box">
                 <h3>CAREER STATS</h3>
-                <p>Podium: {podium}</p>
+                <p>Podium: {podiums}</p>
                 <p>Wins: {wins}</p>
                 <p>Poles: {poles}</p>
                 <p>Wdc: {wdc}</p>
