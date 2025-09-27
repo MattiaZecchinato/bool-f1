fd4c95fec399f221f493b1d7a5d073ed8d0cbf20
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-09-27 03:02:53 +0200

    feat: cerate apiDrivers to manage path /api/drivers

---
+++ frontend/src/services/apiDrivers.js
@@ -0,0 +1,7 @@
+import api from './apiBoolF1'
+
+const getDrivers = () => api.get("/api/drivers")
+
+const getDriverById = (id) => api.get("/api/drivers/" + id);
+
+export default { getDrivers, getDriverById }
