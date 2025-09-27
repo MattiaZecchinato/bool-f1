19d7d1dbcd927bc739e7f499db135afb4f401c9e
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-09-27 03:00:39 +0200

    feat: add BrowserRouter

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,26 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+
+//pages
+import DriversPage from "./pages/DriversPage"
+import NotFoundPage from "./pages/NotFoundPage"
+import DriverDetailsPage from "./pages/DriverDetailsPage"
+
 function App() {
 
   return (
     <>
-
+      <BrowserRouter>
+        <Routes>
+          {/* <Route element={<MainLayout />}> */}
+          {/* <Route path="/" element={<HomePage />} /> */}
+          <Route path="/drivers" element={<DriversPage />} />
+          <Route path="/drivers/:id" element={<DriverDetailsPage />} />
+          {/* <Route path="/teams" element={<TeamsPage />} /> */}
+          {/* <Route path="/teams/:id" element={<TeamDetailsPage />} /> */}
+          <Route path="*" element={<NotFoundPage />} />
+          {/* </Route> */}
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
