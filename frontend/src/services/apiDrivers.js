7f8cd8724556fae57d27bbae8f0a5e7a3a9bcfcb
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-09-29 12:04:16 +0200

    .

--- frontend/src/services/apiDrivers.js
+++ frontend/src/services/apiDrivers.js
@@ -2,6 +2,6 @@ import api from './apiBoolF1'
 
 const getDrivers = () => api.get("/api/drivers")
 
-const getDriverById = (id) => api.get("/api/drivers/" + id);
+const getDriverById = (id) => api.get("/api/drivers/" + id)
 
 export default { getDrivers, getDriverById }
