3e03dda218033e2650dd6de997af136f1121af0f
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-09-29 21:47:00 +0200

    feat: add LoaderContext

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,40 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { useState } from 'react'
+
+// contexts
+import LoaderContext from './contexts/LoaderContext'
 
 // pages
-import DriversPage from "./pages/DriversPage"
-import NotFoundPage from "./pages/NotFoundPage"
-import DriverDetailsPage from "./pages/DriverDetailsPage"
-import TeamsPage from "./pages/TeamsPage"
-import TeamDetailsPage from "./pages/TeamDetailsPage"
-import HomePage from "./pages/HomePage"
+import DriversPage from './pages/DriversPage'
+import NotFoundPage from './pages/NotFoundPage'
+import DriverDetailsPage from './pages/DriverDetailsPage'
+import TeamsPage from './pages/TeamsPage'
+import TeamDetailsPage from './pages/TeamDetailsPage'
+import HomePage from './pages/HomePage'
 
 // layouts
-import MainLayout from "./layouts/MainLayout"
+import MainLayout from './layouts/MainLayout'
 
 function App() {
 
+  const [isLoading, setIsLoading] = useState(false)
+
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<MainLayout />}>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/drivers" element={<DriversPage />} />
-            <Route path="/drivers/:id" element={<DriverDetailsPage />} />
-            <Route path="/teams" element={<TeamsPage />} />
-            <Route path="/teams/:id" element={<TeamDetailsPage />} />
-            <Route path="*" element={<NotFoundPage />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <LoaderContext.Provider value={{ isLoading, setIsLoading }}>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<MainLayout />}>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/drivers" element={<DriversPage />} />
+              <Route path="/drivers/:id" element={<DriverDetailsPage />} />
+              <Route path="/teams" element={<TeamsPage />} />
+              <Route path="/teams/:id" element={<TeamDetailsPage />} />
+              <Route path="*" element={<NotFoundPage />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </LoaderContext.Provider>
     </>
   )
 }
