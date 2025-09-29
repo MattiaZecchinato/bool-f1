70f4db1a20e78b031c24ecd4ab4df9dc9ccad6dc
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-09-29 16:15:59 +0200

    style: change classes name

--- frontend/src/components/DriverDetailsCard.jsx
+++ frontend/src/components/DriverDetailsCard.jsx
@@ -1,23 +1,24 @@
-import '../styles/components/DriverDetailsCard.css'
+// style
+import style from '../styles/components/DriverDetailsCard.module.css'
 
 function DriverDetailsCard({ data }) {
 
     const { firstName, lastName, nationality, carNumber, team, pfpImage } = data
 
     return (
-        <div className="driver-card" style={{ backgroundColor: `var(--${team?.colorName.toLowerCase().replace(' ', '')})` }}>
-            <div className="left-box">
-                <div className="info-box">
-                    <p className="first-name">{firstName}</p>
-                    <p className="last-name text-uppercase">{lastName}</p>
+        <div className={style.driverCard} style={{ backgroundColor: `var(--${team?.colorName.toLowerCase().replace(' ', '')})` }}>
+            <div className={style.leftBox}>
+                <div className={style.infoBox}>
+                    <p className={style.firstName}>{firstName}</p>
+                    <p className={style.lastName}>{lastName}</p>
                 </div>
-                <div className="info-box-2">
+                <div className={style.infoBox2}>
                     <p>{nationality}</p>
                     <p>{team?.name}</p>
                     <p>{carNumber}</p>
                 </div>
             </div>
-            <div className="img-box">
+            <div className={style.imgBox}>
                 <img src={`/img/driverImg/${pfpImage}`} alt={`${firstName} ${lastName}`} />
             </div>
         </div>
