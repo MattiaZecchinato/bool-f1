91d7c1442d25cbd6f0ed273efff2aa21779ec9df
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-09-29 15:16:40 +0200

    feat: create TeamsPage and add TeamCard component

---
+++ frontend/src/pages/TeamsPage.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react"
+
+// components
+import TeamCard from "../components/TeamCard"
+
+// style
+import "../styles/pages/TeamsPage.css"
+
+// api
+import apiTeams from "../services/apiTeams"
+import { Link } from "react-router-dom"
+
+function TeamsPage() {
+
+    const { getTeams } = apiTeams
+
+    const [teams, setTeams] = useState([])
+
+    useEffect(() => {
+        getTeams()
+            .then(res => {
+                console.log(res.data)
+                setTeams(res.data)
+            })
+            .catch(err => {
+                console.log(err)
+                setTeams([])
+            })
+    }, [])
+
+    return (
+        <>
+            <h1>F1 Teams 2025</h1>
+            <div className="teams-container">
+                {teams.length > 0 ? teams.map(team => (
+                    <Link to={`/teams/${team.id}`} key={team.id}>
+                        <TeamCard data={team} />
+                    </Link>
+                )) : (<p>No teams found!</p>)}
+            </div>
+        </>
+    )
+}
+
+export default TeamsPage
