ba09ad4f1b92258a1e1b262d079ab90a6930b3ea
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-10-02 19:00:34 +0200

    feat: remove jsonIgnoreproperty

--- backend/src/main/java/org/boolf1/backend/model/Team.java
+++ backend/src/main/java/org/boolf1/backend/model/Team.java
@@ -5,8 +5,6 @@ import java.util.List;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -45,7 +43,6 @@ public class Team {
     private String colorName;
 
     @OneToMany(mappedBy = "team")
-    @JsonIgnoreProperties({ "team" })
     private List<Driver> drivers;
 
     public Integer getId() {
