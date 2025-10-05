67c138c3ea779cece92e7a1d75200f8a4d01364c
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-10-05 16:08:35 +0200

    feat: add validation annotations for DriverForm fields

--- backend/src/main/java/org/boolf1/backend/model/DriverForm.java
+++ backend/src/main/java/org/boolf1/backend/model/DriverForm.java
@@ -4,19 +4,49 @@ import java.time.LocalDate;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public class DriverForm {
+
     private Integer id;
+
+    @NotBlank(message = "First name must be not blank or empty")
     private String firstName;
+
+    @NotBlank(message = "Last name must be not blank or empty")
     private String lastName;
+
+    @NotNull(message = "Car number must be not null")
     private Integer carNumber;
+
+    @NotNull(message = "Date of birth must be not null")
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate dateBirth;
+
     private String pfpImage;
+
+    @NotBlank(message = "Nationality must be not blank or empty")
     private String nationality;
+
+    @NotNull(message = "Poles positions must be at least 0")
+    @Min(value = 0, message = "The pole positions cannot be negative")
     private Integer poles;
+
+    @NotNull(message = "World Championships must be at least 0")
+    @Min(value = 0, message = "The World Championships cannot be negative")
     private Integer wdc;
+
+    @NotNull(message = "Podiums must be at least 0")
+    @Min(value = 0, message = "The podiums cannot be negative")
     private Integer podiums;
+
+    @NotNull(message = "Wins must be at least 0")
+    @Min(value = 0, message = "The wins cannot be negative")
     private Integer wins;
+
+    @NotNull(message = "You must select a team")
     private Integer teamId;
 
     public Integer getId() {
